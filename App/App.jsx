b9891f0f668ac8ce8100f187373f553c3148559a
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2026-01-12 13:25:11 +0100

    Create initial App component with table structure

---
+++ App/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <table>
+        <tr>
+          <td>ID</td>
+          <td>Tytuł</td>
+          <td>Gatunek</td>
+          <td>Data wydania</td>
+          <td>Licza pól punktowywch</td>
+        </tr>
+        
+      </table>
+    </>
+  )
+}
+
+export default App
